fa383a822d6d4d84dc5f2324ac446e2de2156ed7
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-03-28 14:19:41 +0200

    changed weather icon dynamically

--- src/app/main/shared/current-weather-icon/current-weather-icon.component.ts
+++ src/app/main/shared/current-weather-icon/current-weather-icon.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-current-weather-icon',
   templateUrl: './current-weather-icon.component.html',
-  styleUrls: ['./current-weather-icon.component.scss']
+  styleUrls: ['./current-weather-icon.component.scss'],
 })
 export class CurrentWeatherIconComponent implements OnInit {
+  @Input() type!: 'cloudy' | 'sunny';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
