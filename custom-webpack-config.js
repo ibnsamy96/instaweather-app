5bf38c33bf3d787204cd62463560cd19adc2db4b
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-04-01 16:38:08 +0200

    آني زهجت من لخبطة الديبلويمينت 🚶‍♂️

--- custom-webpack-config.js
+++ custom-webpack-config.js
@@ -4,8 +4,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       $env: {
-        API_KEY: "c2853a6fe518648e65a57eaddd0684a2",
-        LOCATION_API_KEY: "pk.0ff9c4ba4e471c21a43703afe3c207e8",
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        LOCATION_API_KEY: JSON.stringify(process.env.LOCATION_API_KEY),
       },
     }),
   ],
