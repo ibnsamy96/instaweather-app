f9e4fefb862ffc0e3698ad8f56fd6c26ae80cdef
Mahmoud Samy <mahmoudsamy50@outlook.com>
2022-04-09 04:32:09 +0200

    added simple server code

---
+++ backend/dataFetcher.js
@@ -0,0 +1,45 @@
+const fetch = require("node-fetch")
+require("dotenv").config()
+
+const openWeatherMapApi = `https://api.openweathermap.org/data/2.5/weather?`
+const locationIqApi = `https://us1.locationiq.com/v1/reverse.php?`
+const weatherApi = "http://api.weatherapi.com/v1/forecast.json?"
+
+const privateKeys = {
+  open_weather_map: process.env.OPENWEATHERMAP_API_KEY,
+  weather_api: process.env.WEATHERAPI_API_KEY,
+  location_iq: process.env.LOCATIONIQ_API_KEY,
+}
+
+async function fetchJSON(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
+
+async function getOpenWeatherMapInfo(latitude, longitude) {
+  const url =
+    openWeatherMapApi +
+    `lat=${latitude}&lon=${longitude}&units=metric&appid=${privateKeys.open_weather_map}`
+  const data = await fetchJSON(url)
+  return data
+}
+
+async function getWeatherApiInfo(latitude, longitude) {
+  const url =
+    weatherApi + `key=${privateKeys.weather_api}&q=${latitude},${longitude}`
+  const data = await fetchJSON(url)
+  return data
+}
+
+// TODO use location from openweathermap || weatherapi instead of this
+// async function getLocationInfo(latitude, longitude) {
+//   const url =
+//     locationIqApi +
+//     `lat=${latitude}&lon=${longitude}&format=json&normalizeaddress=1&normalizecity=1&key=${privateKeys.location_iq}`
+
+//   const data = await fetchJSON(url)
+//   return data
+// }
+
+module.exports = { getWeatherApiInfo, getOpenWeatherMapInfo }
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,27 @@
-const express = require("express");
+const express = require("express")
+require("dotenv").config()
 
-const app = express();
-const PORT = 3000;
+const { getOpenWeatherMapInfo, getWeatherApiInfo } = require("./dataFetcher")
 
-app.get("/", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.status(200).send("<h1>Hello GFG Learner!</h1>");
-});
+const app = express()
+const PORT = 3000
+
+app.get("/", async (req, res) => {
+  const data = [
+    { "open-weather-map": await getOpenWeatherMapInfo(30.77, 31.0) },
+    { "weather-api": await getWeatherApiInfo(30.77, 31.0) },
+  ]
+
+  // res.set("Content-Type", "text/html")
+  res.status(200).send(data)
+})
 
 app.listen(PORT, (error) => {
+  console.log(process.env.WEATHERAPI_API_KEY)
+
   if (!error)
     console.log(
       "Server is Successfully Running, and App is listening on port " + PORT
-    );
-  else console.log("Error occurred, server can't start", error);
-});
+    )
+  else console.log("Error occurred, server can't start", error)
+})
